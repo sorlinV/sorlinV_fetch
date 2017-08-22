ddfcbc7e74a9dfcc31d7a15607b68b229840c9d2
isidoris <valentin.sorlin@hotmail.com>
2017-08-22 11:50:56 +0200

    update exit test

--- test.js
+++ test.js
@@ -2,6 +2,10 @@ const fetch = require('./fetch.js');
 
 fetch('https://dog.ceo/api/breeds/image/random').then(function(response) {
     console.log(response);
+    if (response !== "") {
+        process.exit(0);
+    }
 }).catch(function(err) {
     console.log(err);
+    process.exit(1);
 });

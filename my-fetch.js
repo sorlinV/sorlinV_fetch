f2bb94f962386b459b882b792c4c9f062bc50656
isidoris <valentin.sorlin@hotmail.com>
2017-08-22 10:16:34 +0200

    npm init, adding fetch.js file and adding Readme

---
+++ my-fetch.js
@@ -0,0 +1,36 @@
+function assignarg(src, target) {
+    for (let key in src) {
+        if (target.hasOwnProperty(key)) {
+            src[key] = target[key];
+        }
+    }
+    return src;
+}
+
+function fetch(url, obj = {}) {
+    return new Promise((resolve, reject) => {
+        let defaults = {
+            method: 'GET',
+            data: undefined,
+            headers: undefined,
+        };
+        defaults = assignarg(defaults, obj);
+        let xhr = new XMLHttpRequest();
+        xhr.open(defaults.method, url, true);
+        xhr.onload = function() {
+            console.log('loaded');
+            if (xhr.status === 200) {
+                resolve(xhr.responseText);
+            }
+        };
+        xhr.onerror = function() {
+            console.log('error');
+            reject(xhr.statusText)
+        };
+        if (defaults.data !== undefined) {
+            xhr.send(defaults.data);
+        } else {
+            xhr.send();
+        }
+    });
+}
